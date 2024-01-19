9c1529757cf4d9a2ace9e0a794b4b1350cd24bce
IsmatQasim <ismat6812@gmail.com>
2024-01-20 00:22:09 +0500

    change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const mainButton = document.getElementsByClassName("main-btn")[0];
 function login(){
-    window.location.href= "login.html"
+    window.location.href= "login.html", " ";
 }
 
 mainButton.addEventListener("click", login);
